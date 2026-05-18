069ec5013b2e1a14cbebe935be6cfc6792324023
yu0814 <yu0814.2021@gmail.com>
2026-05-18 17:31:43 +0900

    バグを修正

--- fog.py
+++ fog.py
@@ -4,16 +4,16 @@ from sklearn.tree import DecisionTreeClassifier
 df = pd.read_csv("data.csv")
 
 X = df[["気温(℃)", "風速(m/s)", "相対湿度(％)"]]
-y = df["蒲生水道橋"]
+y = df["蒲生水道橋"].fillna(0)
 
-model = DecisionTreeClassifier(max_depth=3)
+model = DecisionTreeClassifier(class_weight="balanced", random_state=42)
 model.fit(X, y)
 
-data = pd.DataFrame([[10.0, 0, 0]], columns=["気温(℃)", "風速(m/s)", "相対湿度(％)"])
+new_data = pd.DataFrame(
+    [[24.5, 0.5, 93.0]], columns=["気温(℃)", "風速(m/s)", "相対湿度(％)"]
+)
 
-prediction = model.predict(data)
+prediction = model.predict(new_data)
+print(prediction[0])
 
-if prediction[0] == 1:
-    print("AIの予測：霧が発生する可能性が高いです！")
-else:
-    print("AIの予測：霧は発生しないでしょう。")
+print(pd.read_csv("data.csv")["蒲生水道橋"].unique())
