6a95cd2e3d31ff83cd6ed4d61b924caee2024fce
yu0814 <yu0814.2021@gmail.com>
2026-05-18 16:27:50 +0900

    lightgbmの消去、test.pyの消去

--- fog.py
+++ fog.py
@@ -1,19 +1,17 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 
-df = pd.read_csv("fog_data.csv")
+df = pd.read_csv("data.csv")
 
-X = df[["気温", "湿度", "風速"]]
-y = df["霧発生"]
+X = df[["気温(℃)", "風速(m/s)", "相対湿度(％)"]]
+y = df["蒲生水道橋"]
 
 model = DecisionTreeClassifier(max_depth=3)
 model.fit(X, y)
 
-print("学習が完了しました！")
+data = pd.DataFrame([[10.0, 0, 0]], columns=["気温(℃)", "風速(m/s)", "相対湿度(％)"])
 
-new_data = pd.DataFrame([[10.0, 0, 0]], columns=["気温", "湿度", "風速"])
-
-prediction = model.predict(new_data)
+prediction = model.predict(data)
 
 if prediction[0] == 1:
     print("AIの予測：霧が発生する可能性が高いです！")
